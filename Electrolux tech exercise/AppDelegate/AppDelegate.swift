6d59ac070a0ee5e2caea7a77c1788ba0d30edf45
Igor Rudenko <tamboff69@gmail.com>
2021-07-05 16:01:33 +0300

    initial commit

---
+++ Electrolux tech exercise/AppDelegate/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Electrolux tech exercise
+//
+//  Created by Igor Rudenko on 05.07.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+}
+
---
+++ Electrolux tech exerciseTests/Electrolux_tech_exerciseTests.swift
@@ -0,0 +1,33 @@
+//
+//  Electrolux_tech_exerciseTests.swift
+//  Electrolux tech exerciseTests
+//
+//  Created by Igor Rudenko on 05.07.2021.
+//
+
+import XCTest
+@testable import Electrolux_tech_exercise
+
+class Electrolux_tech_exerciseTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
